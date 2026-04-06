18d2fdd47eac576e0f97eb7aab8f9bd91bd2e613
Brittany Price <bprice0496@gmail.com>
2026-04-05 23:32:56 -0400

    Task 3 pass

--- assignment1/assignment1.py
+++ assignment1/assignment1.py
@@ -6,7 +6,35 @@ def hello():
 
 print(hello)
 
+#Task 2
 def greet(name):
     return(f"Hello, {name}!")
 
 print(greet)
+
+#Task 3
+def calc(first, second, calculate = "multiply"):
+    try:
+        match calculate:
+           case "add":
+            return first + second 
+           case "divide":
+            return first / second
+           case "subtract":
+            return first - second
+           case "modulo":
+            return first % second 
+           case "int_divide":
+            return int(first/second)
+           case "power":
+            return first ^ second
+           case _:
+            return first * second
+        
+    except ZeroDivisionError:
+        return("You can't divide by 0!")
+    
+    except TypeError:
+        return("You can't multiply those values!")
+    
+print(calc)
