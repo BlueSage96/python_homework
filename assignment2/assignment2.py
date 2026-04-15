bf3fbd4db42a4fa923c4bc43351f014f4b8220b7
Brittany Price <bprice0496@gmail.com>
2026-04-15 00:23:55 -0400

    Hopefully last revisions

--- assignment2/assignment2.py
+++ assignment2/assignment2.py
@@ -6,27 +6,30 @@ from datetime import datetime
 
 #Task 1
 def diary():
-    with open("diary.txt", "a") as file:
-      try:
-        prompt1 = input("What happened today? \n")
-        file.write(prompt1 + "\n")  
-        prompt2 = ""
-        
-        while prompt2 != "done for now":
-            prompt2 = input("What else? \n")
-            if prompt2 != "done for now":
-                file.write(prompt2 + "\n")
-                    
-      except Exception as e:
+    try:
+        with open("diary.txt", "a") as file:
+            prompt1 = input("What happened today? \n")
+            file.write(prompt1 + "\n")
+
+            prompt2 = ""
+            while prompt2 != "done for now":
+                prompt2 = input("What else? \n")
+                file.write(prompt2 + "\n")  # now writes "done for now" too
+
+    except Exception as e:
         trace_back = traceback.extract_tb(e.__traceback__)
-        stack_trace = list()
+        stack_trace = []
+
         for trace in trace_back:
-            stack_trace.append(f"File: {trace[0]}, Line: {trace[1]}, Func.Name: {trace[2]}, Message: {trace[3]}")
-            print(f"Exception type: {type(e).__name__}")
-            message = str(e)
+            stack_trace.append(
+                f"File: {trace[0]}, Line: {trace[1]}, Func.Name: {trace[2]}, Message: {trace[3]}"
+            )
+
+        print(f"Exception type: {type(e).__name__}")
+        message = str(e)
         if message:
             print(f"Exception message: {message}")
-            print(f"Stack trace: {stack_trace}")
+        print(f"Stack trace: {stack_trace}")
 diary()
 
 #Task 2
