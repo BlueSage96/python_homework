8c01e0177ee6616d321b20e94580e383a0b7de10
Brittany Price <bprice0496@gmail.com>
2026-04-14 23:27:01 -0400

    Task 1 pass

---
+++ assignment2/assignment2.py
@@ -0,0 +1,28 @@
+import traceback
+
+#Task 1
+def diary():
+    with open("diary.txt", "a") as file:
+      try:
+        prompt1 = input("What happened today? \n")
+        file.write(prompt1 + "\n")  
+        prompt2 = ""
+        
+        while prompt1 != "done for now" and prompt2 != "done for now":
+            prompt2 = input("What else? \n") 
+            file.write(prompt2 + "\n") 
+            
+        else:
+            file.close()
+            
+      except Exception as e:
+        trace_back = traceback.extract_tb(e.__traceback__)
+        stack_trace = list()
+        for trace in trace_back:
+            stack_trace.append(f"File: {trace[0]}, Line: {trace[1]}, Func.Name: {trace[2]}, Message: {trace[3]}")
+            print(f"Exception type: {type(e).__name__}")
+            message = str(e)
+            if message:
+               print(f"Exception message: {message}")
+               print(f"Stack trace: {stack_trace}")
+diary()
