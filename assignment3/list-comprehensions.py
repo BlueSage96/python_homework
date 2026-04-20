7ff36a219dd7160cddd3273af01952bde46ee853
Brittany Price <bprice0496@gmail.com>
2026-04-20 19:00:34 -0400

    Task 3 pass

---
+++ assignment3/list-comprehensions.py
@@ -0,0 +1,19 @@
+import csv
+
+def list_comprehensions(): 
+    with open("../csv/employees.csv", "r") as file:
+        reader = csv.reader(file)
+        employees_list = [x for x in reader]
+
+        employee_names = [
+            y[1] + " " + y[2]
+            for y in employees_list[1:]
+        ]
+        print(f"All employees: {employee_names} \n")
+       
+        employee_names_e = [
+            xe for xe in employee_names 
+            if "e" in xe]
+        print(f"Only employees with e: {employee_names_e} \n")
+        
+list_comprehensions()     
