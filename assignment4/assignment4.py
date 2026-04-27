31f7f42014da9bc718016811df43028841c572e1
Brittany Price <bprice0496@gmail.com>
2026-04-26 22:14:04 -0400

    Formatted print statements

--- assignment4/assignment4.py
+++ assignment4/assignment4.py
@@ -10,7 +10,7 @@ data = {
 
 #Convert to dataframe
 task1_data_frame = pd.DataFrame(data)
-# print(task1_data_frame)
+print("\nIntial data: \n",task1_data_frame)
 
 #save to file
 task1_data_frame.to_csv("task1.csv")
@@ -25,37 +25,38 @@ data = {
 }
 
 task1_with_salary = pd.DataFrame(data)
-# print(task1_with_salary)
+print("\nData with salaries: \n",task1_with_salary)
 task1_older = task1_with_salary.copy()
 
 #Increment age by 1
 updated_task1_older = [26,31,36]
 task1_older["Age"] = pd.Series(updated_task1_older)
-# print(task1_older)
+print("\nIncremented ages by 1: \n",task1_older)
 task1_older.to_csv("employees.csv", index=False)
 
 #Task 2
 task2_employees = pd.read_csv("employees.csv")
-# print(task2_employees)
+print("\nCSV employees: \n",task2_employees)
 json_employees = pd.read_json("additional_employees.json")
-# print(json_employees)
+print("\nJSON employees: \n",json_employees)
 
 more_employees = pd.concat([task2_employees, json_employees],ignore_index=True)
-# print(more_employees)
+print("\nCombined employees: \n",more_employees)
 
 #Task 3
 first_three = more_employees.head(3)
-# print(first_three)
+print("\n First three rows: \n",first_three)
 last_two = more_employees.tail(2)
-# print(last_two)
+print("\nLast two rows: \n",last_two)
 
 employee_shape = more_employees.shape
-# print(employee_shape)
-# more_employees.info()
+print("\n Employee shape: \n",employee_shape)
+print("\n More employees info: \n")
+more_employees.info()
 
 #Task 4
 dirty_data = pd.read_csv("dirty_data.csv")
-# print(dirty_data)
+print("\nInitial dirty data: \n",dirty_data)
 clean_data = dirty_data.copy()
 
 clean_data.drop_duplicates(inplace=True)
@@ -75,4 +76,4 @@ clean_data["Hire Date"] = pd.to_datetime(clean_data["Hire Date"], format="mixed"
 #Strip whitespace & upper case
 clean_data["Name"] = clean_data["Name"].str.strip().str.upper()
 clean_data["Department"] = clean_data["Department"].str.strip().str.upper()
-print(clean_data)
+print("\n Cleaned data: \n",clean_data)
