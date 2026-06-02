4fd85a23ec69c73771c98ccc51c134f206abbd04
Brittany Price <bprice0496@gmail.com>
2026-06-02 00:58:01 -0400

    Task 3 - Created four functions

--- assignment9/sql_intro.py
+++ assignment9/sql_intro.py
@@ -1,20 +1,64 @@
 import sqlite3
 
-#Task 1 - create magazines database
+#Task 1 - create Magazines database
 #Task 2 - create tables
+#Task 3 - populate tables
+
+#Functions for each table
+def create_publisher(cursor, name, year):
+    cursor.execute("SELECT * FROM Publisher WHERE name = ?",(name,)) #search db
+    results = cursor.fetchall() #Get results
+    try:
+        if len(results) > 0:
+            print("Publisher already exists!")
+        else:
+            cursor.execute("INSERT INTO Publisher (name, year) VALUES (?,?)",(name,year))    
+    except sqlite3.IntegrityError:
+        print(f"{name} already exist in the database!")
+
+def create_magazine(cursor, name, year):
+    cursor.execute("SELECT * FROM Magazines WHERE name = ?",(name,)) #search db
+    results = cursor.fetchall() #Get results
+    try:
+        if len(results) > 0:
+            print("Magazine already exists!")
+        else:
+            cursor.execute("INSERT INTO Magazines (name, year) VALUES (?,?)",(name,year))    
+    except sqlite3.IntegrityError:
+        print(f"{name} already exist in the database!")
+        
+def create_subscribers(cursor, name, year):
+    cursor.execute("SELECT * FROM Subscribers WHERE name = ?",(name,)) #search db
+    results = cursor.fetchall() #Get results
+    try:
+        if len(results) > 0:
+            print("Subscribers already exists!")
+        else:
+            cursor.execute("INSERT INTO Subscribers (name, year) VALUES (?,?)",(name,year))    
+    except sqlite3.IntegrityError:
+        print(f"{name} already exist in the database!")
+        
+def create_subscriptions(cursor, subscriber_id, magazine_id, expiration_date):
+    cursor.execute("SELECT * FROM Subscriptions WHERE subscriber_id = ? AND magazine_id = ?",(subscriber_id, magazine_id)) #search db
+
+    results = cursor.fetchall() #Get results
+    try:
+        if len(results) > 0:
+            print("Subscriber and/or magazine and already exists!")
+        else:
+            cursor.execute("INSERT INTO Subscriptions (subscriber_id, magazine_id, expiration_date) VALUES (?,?,?)",(subscriber_id, magazine_id, expiration_date))    
+    except sqlite3.IntegrityError:
+        print(f"Subscription already exist in the database!")
 
 # connect to SQLite database
-with sqlite3.connect("../db/magazines.db") as conn:
+with sqlite3.connect("../db/Magazines.db") as conn:
     try:
+        conn.execute("PRAGMA foreign_keys = 1") #turns on foreign key constraint
         cursor = conn.cursor()
-        #SELECT Students.name, Courses.course_name 
-        # FROM Enrollments
-        # JOIN Students ON Enrollments.student_id = Students.student_id
-        # JOIN Courses ON Enrollments.course_id = Courses.course_id;
         
         #create tables
         cursor.execute("""
-        CREATE TABLE IF NOT EXISTS publishers (
+        CREATE TABLE IF NOT EXISTS Publisher (
             publisher_id INTEGER PRIMARY KEY,
             name TEXT NOT NULL UNIQUE,
             year INTEGER
@@ -22,31 +66,31 @@ with sqlite3.connect("../db/magazines.db") as conn:
         """)
         
         cursor.execute("""
-        CREATE TABLE IF NOT EXISTS magazines (
+        CREATE TABLE IF NOT EXISTS Magazines (
             magazine_id INTEGER PRIMARY KEY,
             name TEXT NOT NULL UNIQUE,
             year INTEGER,
             publisher_id INTEGER,
-            FOREIGN KEY (publisher_id) REFERENCES subscribers (publisher_id)
+            FOREIGN KEY (publisher_id) REFERENCES Publisher (publisher_id)
         )
         """)
         
         cursor.execute("""
-        CREATE TABLE IF NOT EXISTS subscribers (
+        CREATE TABLE IF NOT EXISTS Subscribers (
             subscriber_id INTEGER PRIMARY KEY,
-            name TEXT NOT NULL UNIQUE,
-            address TEXT NOT NULL UNIQUE
+            name TEXT NOT NULL,
+            address TEXT NOT NULL
         )      
         """)
         
         cursor.execute("""
-        CREATE TABLE IF NOT EXISTS subscriptions (
+        CREATE TABLE IF NOT EXISTS Subscriptions (
             subscription_id INTEGER PRIMARY KEY,
-            expiration_date STRING NOT NULL UNIQUE,
+            expiration_date STRING NOT NULL,
             magazine_id INTEGER,
             subscriber_id INTEGER,
-            FOREIGN KEY (magazine_id) REFERENCES magazines (magazine_id),
-            FOREIGN KEY (subscriber_id) REFERENCES subscribers (subscriber_id)
+            FOREIGN KEY (magazine_id) REFERENCES Magazines (magazine_id),
+            FOREIGN KEY (subscriber_id) REFERENCES Subscribers (subscriber_id)
         )       
         """)
         print("Table created successfully")
--- sqlcommand.py
+++ sqlcommand.py
@@ -1,9 +1,9 @@
 import readline  # Provides command line editing and history
 import sqlite3   # For SQL command execution
 import sys
+
 conn = sqlite3.connect("./db/lesson.db",isolation_level='IMMEDIATE')
 conn.execute("PRAGMA foreign_keys = 1")
-
 cursor = conn.cursor()
 
 tables = cursor.execute("SELECT name FROM sqlite_schema WHERE type='table' ORDER BY 'name'").fetchall()
