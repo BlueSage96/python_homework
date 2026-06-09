bbf51f4547ff3fbd0169ae6cf237cd3f83c0193a
Brittany Price <bprice0496@gmail.com>
2026-06-09 18:16:48 -0400

    Task 2 complete

--- assignment10/advanced_sql.py
+++ assignment10/advanced_sql.py
@@ -20,9 +20,32 @@ with sqlite3.connect("../db/lesson.db") as conn:
             """)
             
             for row in task1:
-                print(row)
+                print(f"Task 1: {row}")
             
-        except sqlite3.Error: #Handle Ctrl-D (EOF) gracefully
-            print("\nAn error has occurred...") 
-        
-        conn.close()
+            #Task 2
+            task2 = cursor.execute("""
+                  select customer_name, average_price
+                  from customers
+                  left join (    
+                        select  orders.customer_id as customer_id_b,
+                                avg(c.total_price) as average_price
+                        from    orders
+                        left join (
+                                select      line_items.order_id,
+                                            sum(products.price*line_items.quantity) as total_price
+                                from        products
+                                left join   line_items
+                                on          products.product_id = line_items.product_id 
+                                group by    line_items.order_id
+                        ) as c
+                        on orders.order_id = c.order_id
+                        group by orders.customer_id
+                        order by orders.customer_id
+                   ) as b
+                 on customers.customer_id = b.customer_id_b;                 
+            """)
+            for row in task2:
+                print(f"Task 2: {row}")
+                
+        except sqlite3.Error as e:
+            print(f"\nSQLite error: {e}")
