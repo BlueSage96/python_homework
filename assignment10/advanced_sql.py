b8e568e535bbba7c2848d70d9cf7f5aefcebed58
Brittany Price <bprice0496@gmail.com>
2026-06-09 22:31:34 -0400

    Task 3 complete

--- assignment10/advanced_sql.py
+++ assignment10/advanced_sql.py
@@ -19,33 +19,84 @@ with sqlite3.connect("../db/lesson.db") as conn:
             LIMIT 5;
             """)
             
-            for row in task1:
-                print(f"Task 1: {row}")
+            # for row in task1:
+            #     print(f"Task 1: {row}")
             
             #Task 2
             task2 = cursor.execute("""
-                  select customer_name, average_price
-                  from customers
-                  left join (    
-                        select  orders.customer_id as customer_id_b,
+                  SELECT customer_name, average_price
+                  FROM customers
+                  LEFT JOIN (    
+                        SELECT  orders.customer_id as customer_id_b,
                                 avg(c.total_price) as average_price
-                        from    orders
-                        left join (
-                                select      line_items.order_id,
+                        FROM    orders
+                        LEFT JOIN (
+                                SELECT      line_items.order_id,
                                             sum(products.price*line_items.quantity) as total_price
-                                from        products
-                                left join   line_items
-                                on          products.product_id = line_items.product_id 
-                                group by    line_items.order_id
+                                FROM        products
+                                LEFT JOIN   line_items
+                                ON          products.product_id = line_items.product_id 
+                                GROUP BY    line_items.order_id
                         ) as c
-                        on orders.order_id = c.order_id
-                        group by orders.customer_id
-                        order by orders.customer_id
+                        ON orders.order_id = c.order_id
+                        GROUP BY orders.customer_id
+                        ORDER BY orders.customer_id
                    ) as b
-                 on customers.customer_id = b.customer_id_b;                 
+                 ON customers.customer_id = b.customer_id_b;                 
             """)
-            for row in task2:
-                print(f"Task 2: {row}")
+            # for row in task2:
+            #     print(f"Task 2: {row}")
+            
+            customer = cursor.execute("""
+                SELECT customer_id, customer_name
+                FROM   customers
+                WHERE  customer_name = 'Perez and Sons'
+                
+            """).fetchone() #only need one customer
+            
+            products = cursor.execute("""
+                SELECT product_id, price
+                FROM    products
+                ORDER BY price
+                LIMIT 5                      
+            """).fetchall()
+                 
+            employee = cursor.execute("""
+                  SELECT employee_id, first_name, last_name
+                  FROM   employees
+                  WHERE first_name = 'Miranda' AND last_name = 'Harris'                      
+            """).fetchone()
+            
+
+            order = cursor.execute("""
+                INSERT INTO orders
+                (customer_id, employee_id)
+                VALUES (?, ?)
+                RETURNING order_id
+
+            """, (customer[0], employee[0]))
+
+            order_id = order.fetchone()[0]
+            
+            for product in products:
+                cursor.execute("""
+                    INSERT INTO line_items
+                    (order_id, product_id, quantity)
+                    VALUES (?, ?, ?)
+                """, (order_id, product[0], 10))
+
+            task3 = cursor.execute("""
+                SELECT line_items.line_item_id,
+                    line_items.quantity,
+                    products.product_name
+                FROM line_items
+                JOIN products
+                    ON line_items.product_id = products.product_id
+                WHERE line_items.order_id = ?
+            """, (order_id,))
+            
+            for row in task3:
+                print(row)
                 
         except sqlite3.Error as e:
             print(f"\nSQLite error: {e}")
